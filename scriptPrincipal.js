c9c5457d41a574b81bcc5471b9ec408b52ba5294
MiltonBC <milton_bc9@hotmail.com>
2014-07-31 23:29:04 -0500

    Ultimo
    Agregado fondo

--- scriptPrincipal.js
+++ scriptPrincipal.js
@@ -8,9 +8,4 @@ function configurarApp ()
 	dibujaFooter(canvas,ctx)
 }
 function dibujaFooter(canvas,contexto)
-{
-	contexto.fillStyle = "rgba(0,0,0,0.8)";
-	contexto.moveTo(0,0);
-	contexto.quadraticCurveTo(0,-50,canvas.width-1000,canvas.height-50);
-	contexto.fill();
-}
+{}
